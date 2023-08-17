9c4ef1c7703d261f3a5f00027c650ff1fa81d011
rayhanmfahrezi <rayhanmfahrezi@gmail.com>
2023-08-17 17:23:16 +0700

    add methods in Products.js

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -5,14 +5,25 @@ import {
     createProduct,
     updateProduct,
     deleteProduct,
+    getProductByPrice,
+    getHotDealProducts,
+    getNewCollectionProducts,
+    getMostViewedProducts
 } from "../controllers/Products.js";
+
+import { VerifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/products/:id", getProductById);
-router.post("/products", createProduct);
-router.patch("/products/:id", updateProduct);
-router.delete("/products/:id", deleteProduct);
+router.post("/products", VerifyUser, createProduct);
+router.patch("/products/:id", VerifyUser, updateProduct);
+router.delete("/products/:id", VerifyUser, deleteProduct);
+
+router.get("/products/price", getProductByPrice);
+router.get("/products/hot-deal", getHotDealProducts);
+router.get("/products/new-collection", getNewCollectionProducts);
+router.get("/products/most-viewed", getMostViewedProducts);
 
 export default router;
